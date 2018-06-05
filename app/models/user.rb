9324b69d6e837b284db1d890e36ed16ff8b5b192
yukiraicom <araiyuuki@araiyuukinoMacBook.local>
2018-06-05 14:04:04 +0900

    make db

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :confirmable
+  attr_accessor :group_key
+  belongs_to :group
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -52,4 +52,18 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :enable_starttls_auto => true,
+    :address => "smtp.gmail.com",
+    :port => 587,
+    :domain => 'smtp.gmail.com',
+    :user_name => "profy.test.yukirai@gmail.com", 
+    :password => "CAkBudkijIthmu0", 
+    :authentication => 'login'
+  }
 end

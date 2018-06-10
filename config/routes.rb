989d3cec8ade04f91159835a4aa308d4a984dd24
yukiraicom <yuukiarai19970524@gmail.com>
2018-06-10 18:38:40 +0900

    make view group

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   resources :users, only: [:show]
   resources :users, only: [:show, :edit, :update]
-	root to: "top#index"
+  root to: "top#index"
+  resources :groups, only: [:show, :edit, :update]
 end

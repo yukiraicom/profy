c40c97a1731d16a2868a224c8af509f3e89b3864
yukiraicom <yuukiarai19970524@gmail.com>
2018-06-12 13:32:13 +0900

    回答済み機能追加

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,9 +1,27 @@
 class AnswersController < ApplicationController
+
+  before_action :redirect, only: :new
+
   def new
     @question = Question.find(params[:question_id])
     @answer = Answer.new
     @answer.question_id = @question.id
 
   #5行目のプロパティについてはhttp://melborne.github.io/2012/09/11/understand-ruby-oop-with-js-brain/ を参照
+  end
+
+  def create
+    @answer = Answer.create(create_params)
+  end
+
+  private
+  def create_params
+    params.require(:answer).permit(:question_id, :text).merge(user_id: current_user.id)
+  end
+
+  def redirect
+    if Answer.exists?(question_id: params[:question_id], user_id: current_user.id)
+      redirect_to :root
+    end
   end
 end

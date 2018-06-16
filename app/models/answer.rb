31b6ac2522b089dc5f060553b9c6e8d8fb4dbae6
yukiraicom <yuukiarai19970524@gmail.com>
2018-06-16 18:47:05 +0900

    add feed_content model

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,4 +1,16 @@
 class Answer < ApplicationRecord
   belongs_to :question
   belongs_to :user
+  has_one :feed_content, as: :content, dependent: :destroy
+
+  #callback
+  after_create :create_feed_content
+
+  #validation
+  validates_presence_of :user_id, :text
+  
+  private
+  def create_feed_content
+    self.feed_content = FeedContent.create(group_id: question.group_id, updated_at: updated_at)
+  end
 end
--- app/models/group.rb
+++ app/models/group.rb
@@ -2,4 +2,5 @@ class Group < ApplicationRecord
   #association
   has_many :users
   has_many :questions, ->{ order("created_at DESC") }
+  has_one :feed_content, as: :content, dependent: :destroy
 end
